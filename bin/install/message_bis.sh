f7a552c825a04f8eaae1882fccc9f2e21fe71ff0
pctronique <pctronique@free.fr>
2023-09-22 06:08:21 +0200

    modif texte erreur install

--- bin/install/message_bis.sh
+++ bin/install/message_bis.sh
@@ -1,2 +1,2 @@
-echo "Vérifier la disponibilité des ports dans le fichier '.env'=>'_PORT=' et les modifier si besoin."
-echo "Relancer l'installation '$ ./install.sh'."
+echo "- Vérifier la disponibilité des ports dans le fichier '.env'=>'_PORT=' et les modifier si besoin."
+echo "- Relancer l'installation '$ ./install.sh'."
