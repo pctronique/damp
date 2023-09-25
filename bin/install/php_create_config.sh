e1c2dc0b794b87f967455960fa1c8c390ae9e8a6
pctronique <pctronique@free.fr>
2023-09-25 10:40:55 +0200

    modif bash php create config

--- bin/install/php_create_config.sh
+++ bin/install/php_create_config.sh
@@ -18,21 +18,25 @@ do
    export $line
 done < "$FOLDER_BASE/.env"
 
-REAL_PATH_FILE=$(realpath $0)
-REAL_PATH="${REAL_PATH_FILE/\/php_create_config.sh/}"
+#REAL_PATH_FILE=$(realpath $0)
+#REAL_PATH="${REAL_PATH_FILE/\/php_create_config.sh/}"
+
+cd "${0%/*}/../.."
 
 docker exec $NAME_PROJECT_CONTAINER bash -c "cp /usr/local/etc/php/php.ini-development /var/tmp/php"
-cp "$REAL_PATH/../../projecttmp/tmp/php/php.ini-development" "$REAL_PATH/../../.docker/$DOCKER_FOLDER_PROJECT/php.ini-development"
-cp "$REAL_PATH/../../projecttmp/tmp/php/php.ini-development" "$REAL_PATH/../../.docker/$DOCKER_FOLDER_PROJECT/php.ini"
-patch "$REAL_PATH/../../.docker/$DOCKER_FOLDER_PROJECT/php.ini" < "$REAL_PATH/../../.docker/$DOCKER_FOLDER_PROJECT/php_ini.patch"
+cp "./projecttmp/tmp/php/php.ini-development" "./.docker/$DOCKER_FOLDER_PROJECT/php.ini-development"
+cp "./projecttmp/tmp/php/php.ini-development" "./.docker/$DOCKER_FOLDER_PROJECT/php.ini"
+patch "./.docker/$DOCKER_FOLDER_PROJECT/php.ini" < "./.docker/$DOCKER_FOLDER_PROJECT/php_ini.patch"
 
-sed -i "s/#- .\/.docker\/php\/php.ini:\/usr\/local\/etc\/php\/conf.d\/php.ini:ro/- .\/.docker\/php\/php.ini:\/usr\/local\/etc\/php\/conf.d\/php.ini:ro/" "$REAL_PATH/../../docker-compose.yml" 
+sed -i "s/#- .\/.docker\/php\/php.ini:\/usr\/local\/etc\/php\/conf.d\/php.ini:ro/- .\/.docker\/php\/php.ini:\/usr\/local\/etc\/php\/conf.d\/php.ini:ro/" "./docker-compose.yml" 
 
 docker exec $NAME_SERVER_CONTAINER bash -c "cp /usr/local/apache2/conf/httpd.conf /usr/local/apache2/tmp/httpd"
-cp "$REAL_PATH/../../projecttmp/tmp/httpd/httpd.conf" "$REAL_PATH/../../.docker/apache/httpd.conf-example"
-cp "$REAL_PATH/../../projecttmp/tmp/httpd/httpd.conf" "$REAL_PATH/../../.docker/apache/httpd.conf"
-patch "$REAL_PATH/../../.docker/apache/httpd.conf" < "$REAL_PATH/../../.docker/apache/httpd_conf.patch"
+cp "./projecttmp/tmp/httpd/httpd.conf" "./.docker/apache/httpd.conf-example"
+cp "./projecttmp/tmp/httpd/httpd.conf" "./.docker/apache/httpd.conf"
+patch "./.docker/apache/httpd.conf" < "./.docker/apache/httpd_conf.patch"
 
-sed -i "s/#- .\/.docker\/apache\/httpd.conf:\/usr\/local\/apache2\/conf\/httpd.conf/- .\/.docker\/apache\/httpd.conf:\/usr\/local\/apache2\/conf\/httpd.conf/" "$REAL_PATH/../../docker-compose.yml"
+sed -i "s/#- .\/.docker\/apache\/httpd.conf:\/usr\/local\/apache2\/conf\/httpd.conf/- .\/.docker\/apache\/httpd.conf:\/usr\/local\/apache2\/conf\/httpd.conf/" "./docker-compose.yml"
+
+cd "${0%/*}"
 
 exit 0
