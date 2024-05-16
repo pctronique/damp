e60e09797157df47830a5c899b1840a4cb62e528
pctronique <pctronique@free.fr>
2024-05-16 16:39:14 +0200

    add logo

--- phpunit/www/index.php
+++ phpunit/www/index.php
@@ -8,8 +8,22 @@ require __DIR__ . "/config/connexionsgbd.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello world!</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        h1 {
+            margin: 20px 0px;
+        }
+        img {
+            width: 250px;
+        }
+    </style>
 </head>
 <body>
     <h1>Hello world!</h1>
+    <img src="./logo.svg" alt="logo">
 </body>
 </html>
